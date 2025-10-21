1bdf2942e79ad281ac0968f51bfc4d781371837b
Denis-Android-Git <124072945+Denis-Android-Git@users.noreply.github.com>
2025-10-21 22:46:26 +0300

    n

---
+++ core/designsystem/src/commonMain/kotlin/com/example/designsystem/components/textfields/MyPasswordTextField.kt
@@ -0,0 +1,171 @@
+package com.example.designsystem.components.textfields
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.BasicSecureTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.foundation.text.input.TextObfuscationMode
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.ripple
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import cmpcourseapp.core.designsystem.generated.resources.Res
+import cmpcourseapp.core.designsystem.generated.resources.eye_icon
+import cmpcourseapp.core.designsystem.generated.resources.eye_off_icon
+import cmpcourseapp.core.designsystem.generated.resources.hide_password
+import cmpcourseapp.core.designsystem.generated.resources.show_password
+import com.example.designsystem.theme.ChirpTheme
+import com.example.designsystem.theme.extended
+import org.jetbrains.compose.resources.stringResource
+import org.jetbrains.compose.resources.vectorResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun MyPasswordTextField(
+    modifier: Modifier = Modifier,
+    state: TextFieldState,
+    isPasswordVisible: Boolean,
+    onVisibilityClick: () -> Unit,
+    placeholder: String? = null,
+    title: String? = null,
+    supportingText: String? = null,
+    isError: Boolean = false,
+    enabled: Boolean = true,
+    onFocusChanged: (Boolean) -> Unit = {}
+) {
+    MyTextFieldLayOut(
+        modifier = modifier,
+        title = title,
+        isError = isError,
+        supportingText = supportingText,
+        enabled = enabled,
+        onFocusChanged = onFocusChanged
+    ) { styleModifier, interactionSource ->
+        BasicSecureTextField(
+            state = state,
+            modifier = styleModifier,
+            enabled = enabled,
+            textObfuscationMode = if (isPasswordVisible) TextObfuscationMode.Visible else TextObfuscationMode.Hidden,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            textStyle = MaterialTheme.typography.bodyMedium.copy(
+                color = if (enabled) MaterialTheme.colorScheme.onSurface else MaterialTheme.colorScheme.extended.textPlaceholder
+            ),
+            interactionSource = interactionSource,
+            cursorBrush = SolidColor(MaterialTheme.colorScheme.onSurface),
+            decorator = { decorator ->
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Box(
+                        modifier = Modifier.weight(1f),
+                        contentAlignment = Alignment.CenterStart
+                    ) {
+                        if (state.text.isEmpty() && placeholder != null) {
+                            Text(
+                                text = placeholder,
+                                color = MaterialTheme.colorScheme.extended.textPlaceholder,
+                                style = MaterialTheme.typography.bodyMedium
+                            )
+                        } else {
+                            decorator()
+                        }
+                    }
+                    Icon(
+
+                        imageVector = if (isPasswordVisible) {
+                            vectorResource(Res.drawable.eye_off_icon)
+                        } else {
+                            vectorResource(Res.drawable.eye_icon)
+                        },
+                        contentDescription = if (isPasswordVisible) {
+                            stringResource(Res.string.hide_password)
+                        } else {
+                            stringResource(Res.string.show_password)
+                        },
+                        tint = MaterialTheme.colorScheme.extended.textDisabled,
+                        modifier = Modifier
+                            .size(24.dp)
+                            .clickable(
+                                interactionSource = remember { MutableInteractionSource() },
+                                indication = ripple(
+                                    bounded = false,
+                                    radius = 24.dp
+                                ),
+                                onClick = onVisibilityClick
+                            )
+                    )
+                }
+            }
+        )
+    }
+}
+
+
+@Composable
+@Preview(showBackground = true)
+fun MyPasswordTextFieldPreview() {
+    ChirpTheme {
+        MyPasswordTextField(
+            modifier = Modifier.width(300.dp),
+            state = rememberTextFieldState("password123"),
+            placeholder = "Placeholder",
+            title = "Title",
+            supportingText = "Supporting text",
+            isError = false,
+            onFocusChanged = {},
+            isPasswordVisible = false,
+            onVisibilityClick = {}
+        )
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun MyPasswordTextFieldEmptyPreview() {
+    ChirpTheme {
+        MyPasswordTextField(
+            modifier = Modifier.width(300.dp),
+            state = rememberTextFieldState("sdsds"),
+            placeholder = "Placeholder",
+            title = "Title",
+            supportingText = "Supporting text",
+            isError = false,
+            onFocusChanged = {},
+            isPasswordVisible = true,
+            onVisibilityClick = {}
+        )
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun MyPasswordTextFieldErrorPreview() {
+    ChirpTheme {
+        MyPasswordTextField(
+            modifier = Modifier.width(300.dp),
+            state = rememberTextFieldState("password123"),
+            placeholder = "Placeholder",
+            title = "Title",
+            supportingText = "No uppercase letter",
+            isError = true,
+            onFocusChanged = {},
+            isPasswordVisible = true,
+            onVisibilityClick = {}
+        )
+    }
+}
--- core/designsystem/src/commonMain/kotlin/com/example/designsystem/components/textfields/MyTextField.kt
+++ core/designsystem/src/commonMain/kotlin/com/example/designsystem/components/textfields/MyTextField.kt
@@ -1,17 +1,8 @@
 package com.example.designsystem.components.textfields
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.input.TextFieldLineLimits
@@ -20,9 +11,7 @@ import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.input.KeyboardType
@@ -44,21 +33,14 @@ fun MyTextField(
     keyboardType: KeyboardType = KeyboardType.Text,
     onFocusChanged: (Boolean) -> Unit = {},
 ) {
-    val interActionSource = remember { MutableInteractionSource() }
-    val isFocused by interActionSource.collectIsFocusedAsState()
-    LaunchedEffect(isFocused) {
-        onFocusChanged(isFocused)
-    }
-    Column(
-        modifier = modifier
-    ) {
-        if (title != null) {
-            Text(
-                title, style = MaterialTheme.typography.labelSmall,
-                color = MaterialTheme.colorScheme.extended.textSecondary
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-        }
+    MyTextFieldLayOut(
+        modifier = modifier,
+        title = title,
+        isError = isError,
+        supportingText = supportingText,
+        enabled = enabled,
+        onFocusChanged = onFocusChanged
+    ) { style, interActionSource ->
         BasicTextField(
             state = state,
             enabled = enabled,
@@ -71,29 +53,11 @@ fun MyTextField(
             ),
             cursorBrush = SolidColor(MaterialTheme.colorScheme.onSurface),
             interactionSource = interActionSource,
-            modifier = Modifier.fillMaxWidth()
-                .background(
-                    color = when {
-                        isFocused -> MaterialTheme.colorScheme.primary.copy(alpha = 0.05f)
-                        enabled -> MaterialTheme.colorScheme.surface
-                        else -> MaterialTheme.colorScheme.extended.secondaryFill
-                    },
-                    shape = RoundedCornerShape(8.dp)
-                )
-                .border(
-                    width = 1.dp,
-                    color = when {
-                        isError -> MaterialTheme.colorScheme.error
-                        isFocused -> MaterialTheme.colorScheme.primary
-                        else -> MaterialTheme.colorScheme.outline
-                    },
-                    shape = RoundedCornerShape(8.dp)
-                )
-                .padding(12.dp),
+            modifier = style,
             decorator = { innerBox ->
                 Box(
                     modifier = Modifier.fillMaxWidth(),
-                    contentAlignment = androidx.compose.ui.Alignment.CenterStart
+                    contentAlignment = Alignment.CenterStart
                 ) {
                     if (state.text.isEmpty() && placeholder != null) {
                         Text(
@@ -107,14 +71,6 @@ fun MyTextField(
                 }
             }
         )
-        if (supportingText != null) {
-            Spacer(modifier = Modifier.height(4.dp))
-            Text(
-                text = supportingText,
-                color = if (isError) MaterialTheme.colorScheme.error else MaterialTheme.colorScheme.extended.textTertiary,
-                style = MaterialTheme.typography.bodySmall
-            )
-        }
     }
 }
 
